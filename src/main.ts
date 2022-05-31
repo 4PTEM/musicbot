fb9693ed9e960eed7712f9895f9b72a4be26082d
MamkinHazker <zeynchan@gmail.com>
2022-05-31 14:09:31 +0300

    deleted test action

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
 import { Client, Intents, MessageAttachment } from 'discord.js';
 import 'dotenv/config';
-import * as fs from 'fs';
-import { ActionsPlanner, EveryDayAction } from './actionsPlanner';
 import { commands } from './commands';
 import { BOT_TOKEN } from './constants';
 import { Handler } from './handler';
@@ -12,16 +10,6 @@ client.on('ready', async () => {
     await client.application?.commands.fetch();
     if (!client.user) throw new Error('authentication error');
     console.log(`Logged in as ${client.user.tag}!`);
-    const actionsPlanner = new ActionsPlanner();
-    actionsPlanner.addAction(
-        new EveryDayAction('07:00:00', async () => {
-            const sashachat = await client.users.createDM('678313025161396245');
-            const photo = new MessageAttachment(fs.readFileSync('./images/goodMorningSasha.jpg'), 'good_morning.jpg');
-            sashachat.send({
-                files: [photo],
-            });
-        })
-    );
     const handler = new Handler(client, commands);
     client.on('interactionCreate', async (interaction) => {
         if (!client.user) throw new Error('No client user');
