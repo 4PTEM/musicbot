9f94d19dbd46469a591aeca8d923fe4d820cf65b
4PTEM <zeynchan@gmail.com>
2022-12-24 07:56:59 +0300

    Command init fix

--- src/handler.ts
+++ src/handler.ts
@@ -1,6 +1,6 @@
-import { ApplicationCommand, Client, Collection, CommandInteraction, CommandInteractionOptionResolver, REST, Routes } from 'discord.js';
-import { Command } from './commands/types';
 import Bottleneck from 'bottleneck';
+import { Client, CommandInteraction, REST, Routes } from 'discord.js';
+import { Command } from './commands/types';
 import { BOT_TOKEN } from './constants';
 
 export class Handler {
@@ -20,8 +20,9 @@ export class Handler {
         const rest = new REST({ version: '10' }).setToken(BOT_TOKEN!);
         try {
             console.log('(HANDLER)[INFO] Initialazing commands');
-            await rest.put(Routes.applicationCommands(this.client.application.id), { body: commands });
+            await rest.put(Routes.applicationCommands(this.client.application.id), { body: commands.map((command) => command.data) });
             console.log('(HANDLER)[INFO] Commands initialized');
+            commands.forEach((command) => this.commands.set(command.data.name, command));
         } catch (error) {
             console.log(error);
         }
